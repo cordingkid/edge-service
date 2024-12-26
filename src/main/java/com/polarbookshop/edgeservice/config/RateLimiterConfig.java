a5b76079f85f9f8c4ac3296ee90c2f35a51b82b9
codingkid <jjg1014@naver.com>
2024-12-26 21:41:30 +0900

    feat: 각 사용자에 대한 사용률 제한 설정

--- src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
@@ -15,6 +15,6 @@ public class RateLimiterConfig {
         // 요청에 대한 사용률 제한은 상수 키를 사용해 적용
         return exchange -> exchange.getPrincipal()
                 .map(Principal::getName)
-                .defaultIfEmpty("anonymous");
+                .defaultIfEmpty("anonymous");   // 요청이 인증되지 않으면 사용율 제한을 적용하기 위한 기본키 값으로 anonymous를 사용
     }
 }
