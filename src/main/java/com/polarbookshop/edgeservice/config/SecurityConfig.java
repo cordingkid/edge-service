3bb52224be7d8760819a114b306d17f78da90c0d
codingkid <jjg1014@naver.com>
2025-01-02 22:04:21 +0900

    feat: RP 주도 로그아웃 및 리다이렉션 설정

--- src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -4,15 +4,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http){
+    SecurityWebFilterChain springSecurityFilterChain(
+            ServerHttpSecurity http,
+            ReactiveClientRegistrationRepository clientRegistrationRepository
+    ){
         return http.authorizeExchange(exchange -> exchange.anyExchange().authenticated())       // 모든 요청에 대해 인증 해야함
                 .oauth2Login(Customizer.withDefaults())                                         // OAuth2/오픈ID 커넥트를 사용한 사용자 인증 활성화
+                .logout(logout -> logout.logoutSuccessHandler(
+                        oidcLogoutSuccessHandler(clientRegistrationRepository)))
                 .build();
+    }
+
+    private ServerLogoutSuccessHandler oidcLogoutSuccessHandler(ReactiveClientRegistrationRepository clientRegistrationRepository) {
+        var oidcLogoutSuccessHandler = new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository);
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");                         // 애플리케이션 베이스 URL로 리다이렉션
+        return oidcLogoutSuccessHandler;
     }
 }
