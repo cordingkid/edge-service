bf97a4d9ab0054eb7cf3d93665d00eda5bf504e8
codingkid <jjg1014@naver.com>
2025-01-09 21:39:07 +0900

    feat: SPA에 대한 쿠키 기반 전략을 위해 CSRF 설정

--- src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -10,9 +10,16 @@ import org.springframework.security.oauth2.client.registration.ReactiveClientReg
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.server.csrf.CsrfToken;
+import org.springframework.security.web.server.csrf.XorServerCsrfTokenRequestAttributeHandler;
+import org.springframework.web.server.WebFilter;
+import reactor.core.publisher.Mono;
 
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
+// @EnableWebFluxSecurity를 사용하면 자동으로 다른 스프링 시큐리티 기능들을 사용해서 "/" 를 요청해도 리다이렉션 됫던것
+// @Configuration(proxyBeanMethods = false) 이 어노테이션을 사용해서 해당 클래스에서만 정의한 기능만 빈으로 등록되게 해서 해결
 //@EnableWebFluxSecurity 이거 주석 처리시 permitAll() 무시되는 문제 해결
 @Configuration(proxyBeanMethods = false)
 public class SecurityConfig {
@@ -28,9 +35,12 @@ public class SecurityConfig {
                         .anyExchange().authenticated())       // 그 외 다른 요청은 사용자 인증 필요
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         .authenticationEntryPoint(new HttpStatusServerEntryPoint(UNAUTHORIZED))) // 사용자가 인증되지 않을경우 401 반환
-                .oauth2Login(Customizer.withDefaults())                                         // OAuth2/오픈ID 커넥트를 사용한 사용자 인증 활성화
+                .oauth2Login(Customizer.withDefaults())                                          // OAuth2/오픈ID 커넥트를 사용한 사용자 인증 활성화
                 .logout(logout -> logout.logoutSuccessHandler(
                         oidcLogoutSuccessHandler(clientRegistrationRepository)))
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())   // CSRF토큰을 교환하기위해 쿠기 기반 방식 사용
+                        .csrfTokenRequestHandler(new XorServerCsrfTokenRequestAttributeHandler()::handle))
                 .build();
     }
 
@@ -39,4 +49,22 @@ public class SecurityConfig {
         oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");                         // 애플리케이션 베이스 URL로 리다이렉션
         return oidcLogoutSuccessHandler;
     }
+
+    /**
+     * CsrfToken 리액티브 스트림을 구독하고
+     * 이 토큰의 값을 올바르게 추출하기 위한 목적만을 갖는 필터
+     * @return
+     */
+    @Bean
+    WebFilter csrfWebFilter() {
+        return (exchange, chain) -> {
+            exchange.getResponse()
+                    .beforeCommit(() -> Mono.defer(() -> {
+                        Mono<CsrfToken> csrfToken = exchange.getAttribute(CsrfToken.class.getName());
+                        return csrfToken != null ? csrfToken.then() : Mono.empty();
+                    }));
+            return chain.filter(exchange);
+        };
+    }
+
 }
